c2847fef7437ec2f26abd58b3bce1507b8f1d2eb
Nikita Osotskiy <osotskiy@gmail.com>
2018-09-17 13:16:46 +0300

    add constructor to App class, change render structure;

--- src/App.js
+++ src/App.js
@@ -3,18 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      header: {},
+      sidebar: {},
+      footer: {}
+    }
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return ([
+      <Header logo={logo}/>,
+      <main className="main">
+        <article className="main-menu">
+          <Sidebar />
+          <aside className="switch-content"> </aside>
+        </article>
+      </main>,
+      <Footer />
+    ]);
   }
 }
 
