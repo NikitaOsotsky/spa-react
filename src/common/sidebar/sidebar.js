ced260a05616497fa01f2eb095368cc1186a722f
Mikita Asotski <masotski@exadel.com>
2018-09-18 14:42:51 +0300

    add constructor(); edit render();

--- src/common/sidebar/sidebar.js
+++ src/common/sidebar/sidebar.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 
 class Sidebar extends Component {
+  constructor(props) {
+    super(props);
+    this.items = props.items.map((item)=>
+      <li key={item}>{item}</li>
+    )
+  }
+
   render() {
     return (
       <aside className="sidebar">
-
+          <section>
+          <h3>{this.props.headline}</h3>
+          <ul>{this.items}</ul>
+        </section>
       </aside>
     );
   }
