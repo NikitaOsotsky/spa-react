9faebe90c8986ba29315328a963fc4080a15dbe9
Mikita Asotski <masotski@exadel.com>
2018-09-19 10:53:32 +0300

    add footer structure;

--- src/common/footer/footer.js
+++ src/common/footer/footer.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import './footer.css';
 
 class Footer extends Component {
   render() {
     return (
-      <footer>
-
+      <footer className="footer">
+        <section className="top-footer">
+          <img className="spinner" src={this.props.logo} alt="logo"/>
+          <h4>{this.props.headline}</h4>
+        </section>
+        <section className="bottom-footer">
+        </section>
       </footer>
     );
   }
