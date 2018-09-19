c85d1addece4175a9787b9923b823fb28d70d9e8
Mikita Asotski <masotski@exadel.com>
2018-09-19 12:49:53 +0300

    fixes;

--- src/common/content/content.js
+++ src/common/content/content.js
@@ -3,13 +3,23 @@ import {
   Route,
 } from "react-router-dom";
 import './content.css';
-import Home from "../dinamic_content/home";
+import Home from "../../dinamic_content/home";
+import Docs from "../../dinamic_content/docs";
+import Examples from "../../dinamic_content/examples";
+import About from "../../dinamic_content/about";
+
+const obj = {
+  Home: Home,
+  Docs: Docs,
+  Examples: Examples,
+  About: About
+};
 
 class Content extends Component {
   constructor(props) {
     super(props);
     this.items = props.items.map((item)=>
-      <Route key={item} path={"/"+item} component={Home} /> /*exact*/
+      <Route key={item} path={"/"+item} component={obj[item]} /> /*exact*/
     )
   }
 
