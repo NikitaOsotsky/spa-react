981a05640337948d56e026bc4df026b966a75cb1
Mikita Asotski <masotski@exadel.com>
2018-09-19 12:03:38 +0300

    edit import and render;

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  HashRouter
+} from "react-router-dom";
 import Header from "./common/header/header";
 import Footer from "./common/footer/footer";
 import Sidebar from "./common/sidebar/sidebar";
+import Content from "./common/content/content"
 
 class App extends Component {
   constructor() {
@@ -22,16 +26,20 @@ class App extends Component {
   };
 
   render() {
-    return ([
-      <Header logo={logo} headline={this.state.header.headline} key="header"/>,
+    return (
+      <HashRouter>
+        <div>
+      <Header logo={logo} headline={this.state.header.headline} key="header"/>
       <main className="main" key="main">
         <article className="main-menu">
           <Sidebar headline={this.state.sidebar.headline} items={this.state.sidebar.items}/>
-          <aside className="switch-content"> </aside>
+          <Content headline={this.state.sidebar.headline} items={this.state.sidebar.items}/>
         </article>
-      </main>,
+      </main>
       <Footer headline={this.state.header.headline} logo={logo} key="footer"/>
-    ]);
+        </div>
+      </HashRouter>
+        );
   }
 }
 
