eb72083eb10ec0153328f75c7b1d3aff28ba0048
Mikita Asotski <masotski@exadel.com>
2018-09-19 12:01:48 +0300

    add simple dinamic content;

---
+++ src/common/dinamic_content/home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './home.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+        <p>Page Home!</p>
+      </div>
+    );
+  }
+
+}
+
+export default Home;
