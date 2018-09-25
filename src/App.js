6da06486809361bede3f7ff894f4d1e70ccf04fc
Mikita Asotski <masotski@exadel.com>
2018-09-25 14:21:15 +0300

    add new menu item - News;

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Header from "./common/header/header";
 import Footer from "./common/footer/footer";
 import Sidebar from "./common/sidebar/sidebar";
 import Content from "./common/content/content"
-import { Home, Docs, Examples, About } from "./importer";
+import { Home, Docs, Examples, About, News } from "./importer";
 
 class App extends Component {
   constructor() {
@@ -19,13 +19,14 @@ class App extends Component {
       },
       sidebar: {
         headline: "Menu:",
-        items: ["Home", "Docs", "Examples", "About"]
+        items: ["Home", "Docs", "Examples", "About", "News"]
       },
       content: {
         Home: Home,
         Docs: Docs,
         Examples: Examples,
-        About: About
+        About: About,
+        News: News
       },
       footer: {}
     }
