e07fcce9c60a667ca27a76a4680d4686c18e522e
Mikita Asotski <masotski@exadel.com>
2018-09-26 14:04:41 +0300

    del state; checkout to prev;

--- src/common/content/content.js
+++ src/common/content/content.js
@@ -8,49 +8,22 @@ class Content extends Component {
   constructor(props) {
     const obj = props.pagesName;
     super(props);
-    this.state = {
-      data: []
-    };
     this.items = props.items.map((item)=> {
       if (item === 'Home') {
         return   <Route key={item} exact path={"/"} component={obj[item]} /> /*exact*/
       }
-      if (item === 'News') {
-        this.loadData();
-      }
       return   <Route key={item} path={"/"+item} component={obj[item]} /> /*exact*/
       }
     );
-
-    console.log(this.items);
-    console.log(this.props.requestAddress);
-  }
-
-  loadData() {
-    fetch(this.props.requestAddress)
-      .then((response) => {
-        console.log(response.headers.get('Content-Type'));
-        console.log(response.status);
-
-        return response.json();
-      })
-      .then((data) => {
-        this.setState(() => {
-          return {data: data};
-        });
-      })
-      .catch( console.log );
   }
 
   shouldComponentUpdate() {
-    console.log("shouldComponentUpdate run");
-    console.log(this.state);
+    console.log("shouldComponentUpdate in Content run");
     return true;
   }
 
   render() {
-    console.log("render run");
-    console.log(this.state);
+    console.log("render in Content run");
     return (
       <aside className="switch-content">
         <p>Generic content below!</p>
