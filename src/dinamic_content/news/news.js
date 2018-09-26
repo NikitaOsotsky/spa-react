2d961e40d0427fdc45baced677010ff4a8bd407e
Mikita Asotski <masotski@exadel.com>
2018-09-26 14:05:03 +0300

    new logic;

--- src/dinamic_content/news/news.js
+++ src/dinamic_content/news/news.js
@@ -2,7 +2,38 @@ import React, { Component } from 'react';
 import './news.css';
 
 class News extends Component {
+  constructor(props) {
+    super(props);
+    this.loadData();
+    this.state = {
+      data: undefined
+    };
+  }
+
+  loadData() {
+    fetch("http://localhost:3004/data")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.setState(() => {
+          return {data: data};
+        });
+      })
+      .catch( console.log );
+  }
+
+  shouldComponentUpdate() {
+    console.log("shouldComponentUpdate in News run");
+
+    return true;
+  }
+
   render() {
+    console.log("render in News run");
+    console.log(this.state.data);
+    if (!this.state.data) return null;
+
     return (
       <div className="news">
 
